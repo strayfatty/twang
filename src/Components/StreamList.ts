6221f28a675010bda463aaae4615cbd7f60f007b
Thomas Meyer <strayfatty@gmail.com>
2022-03-04 09:54:43 +0100

    header styling

--- src/Components/StreamList.ts
+++ src/Components/StreamList.ts
@@ -7,11 +7,17 @@ export class StreamList implements m.Component<StreamListModel> {
     view(vnode: m.Vnode<StreamListModel>) {
         const model = vnode.attrs;
         const streams = model.streams || [];
-        return m(".stream-list", [
+        return m(".stream-list", {
+            class: model.streams == null
+                ? "is-loading"
+                : streams.length === 0 ? "is-empty" : null
+        }, [
             m("a.stream-list__title", {
                 href: model.url,
                 target: "_blank"
             }, model.title),
+            m(".stream-list__loading"),
+            m(".stream-list__empty", "no streams found"),
             m(".stream-list__streams", streams.map(stream => m(StreamCard, stream)))
         ]);
     }
