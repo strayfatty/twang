7a2f2f214fd64a9fdec66c1b51fb00048ce6d7a0
Thomas Meyer <strayfatty@users.noreply.github.com>
2025-11-13 00:29:06 +0100

    Remove analytics (#14)

--- site/index.tsx
+++ site/index.tsx
@@ -1,9 +1,7 @@
 import m from "mithril";
 import { Dashboard } from "~/components/dashboard";
-import { injectAnalytics } from "~/lib/injectAnalytics";
 import { Layout } from "~/pages/layout";
 
-injectAnalytics();
 m.mount(document.body, {
     view: () => (
         <Layout>
